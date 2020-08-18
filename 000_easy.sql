1408528311a4d18fab2262d9596974e5a7675f8c
Evedel <svbiriukov@gmail.com>
2020-08-18 16:01:03 +1000

    day 11

--- 000_easy.sql
+++ 000_easy.sql
@@ -90,3 +90,20 @@ FROM station
 WHERE lat_n > 38.7780
 ORDER BY lat_n
 LIMIT 1
+
+/*
+	Query the greatest value of the Northern Latitudes (LAT_N) from STATION
+	that is less than 137.2345.
+	Truncate your answer to 4 decimal places.
+*/
+SELECT ROUND(MAX(lat_n),4)
+FROM station
+WHERE lat_n < 137.2345
+
+/*
+	Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780.
+	Round your answer to 4 decimal places.
+*/
+SELECT ROUND(MIN(lat_n),4)
+FROM station
+WHERE lat_n > 38.7780
